7b9a08ffad50e3b638fd1de1d13bc92045e8c7e4
gabe <gmaggiotti@gmail.com>
2017-06-12 11:21:04 -0300

    some refactor

--- ab-cache-breaker.js
+++ ab-cache-breaker.js
@@ -5,13 +5,22 @@ var url = require("url");
 var host;
 var count;
 var concurrency;
-var device="desktop";
+var device = "desktop";
+
+function mean(array) {
+    return array.reduce(function (p, c) {
+            return p + c;
+        }) / array.length;
+}
 
 
 function curl(url, options) {
     process.argv;
-    fetch(url,options).catch(function(err) {
-        console.log("process has stopped: "+err);
+    let curTime = (new Date()).getTime();
+    return fetch(url, options).then(() => {
+        return (new Date()).getTime() - curTime;
+    }).catch(function (err) {
+        console.log("process has stopped: " + err);
         process.exit(-1);
     });
 }
@@ -19,7 +28,7 @@ function curl(url, options) {
 function usage() {
     console.log("Usage: ./ab-cache-breaker -n <requests> -c <concurrency> <url>");
     console.log("Options are:");
-    console.log("    -n requests     number of requests to perform" );
+    console.log("    -n requests     number of requests to perform");
     console.log("    -c concurrency  number of multiple requests to make at a time");
     console.log("    -d              device input can be 'desktop' or 'mobile'")
 
@@ -32,24 +41,27 @@ function parseArguments() {
         process.exit(-1);
     }
 
-    if(arg[2] != "-n") {
+    if (arg[2] != "-n") {
         usage()
         process.exit(-1);
     }
 
-    if(arg[4] != "-c" || parseInt(arg[3]) < parseInt(arg[5])) {
+    if (arg[4] != "-c" || parseInt(arg[3]) < parseInt(arg[5])) {
         usage()
         process.exit(-1);
     }
 
-    if( (res = arg.findIndex((val)=>{return val=="-d"})) != -1 ){
+    if ((res = arg.findIndex((val) => {
+            return val == "-d"
+        })) != -1) {
         this.device = arg[res + 1];
         console.log("*****" + this.device);
     }
 
     this.host = arg[6]
-    this.count= arg[3];
-    this.concurrency = arg[5];;
+    this.count = arg[3];
+    this.concurrency = arg[5];
+    ;
 
 }
 
@@ -59,28 +71,27 @@ function abCacheBraker() {
     var j = 0;
     let options = {};
     for (i = 0; i < this.count; i++) {
-        if(this.device == "mobile")
+        if (this.device == "mobile")
             options = {
                 headers: {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Mobile Safari/537.36'}
-        };
+            };
 
-        proms[j++] = new Promise((resolve, reject) => {
-            let url_breaker = "?a" + Math.floor(Math.random() * 10) + "=" + Math.floor(Math.random() * 100000);
-            curl(this.host + url_breaker, options);
-            resolve();
-        });
+        let url_breaker = "?a" + Math.floor(Math.random() * 10) + "=" + Math.floor(Math.random() * 100000);
+        proms[j++] = curl(this.host + url_breaker, options);
 
         if (i % this.concurrency == 0 && i > 0) {
             Promise.all(proms).catch(function (e) {
                 console.log(e);
+            }).then((response) => {
+                console.log("Completed " + i + " requests in " + mean(response) + "ms.");
             });
-            console.log("Completed " + i + " requests.");
             j = 0;
         } else if (i == this.count - 1) {
             Promise.all(proms).catch(function (e) {
                 console.log(e);
+            }).then((response) => {
+                console.log("Completed " + (i + 1) + " requests in " + mean(response) + "ms.");
             });
-            console.log("Completed " + (i + 1) + " requests.");
             j = 0;
         }
     }
